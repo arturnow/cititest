4748e946a0db7f832efd97cde8e488cf2be11fd8
Artur Nowocin <artur.nowocin@gmail.com>
2015-11-28 23:21:40 +0100

    Need some refactoring

--- BrokerageService.js
+++ BrokerageService.js
@@ -41,24 +41,19 @@ BrokerageService.prototype.setOrder = function(orderedNumber){
 		console.info('To many coins ordered');
 		return false;
 	}	
-	//Musimy zrobić ruchmą usługę - dla <=110 możemy zlecać od 10 - 100 do 100 - 10
+
 	var maxMoves = orderedNumber / 10;
 	var minQuote = undefined;
 	
-	//Dla 130 maxMoves = 13! ale mamy max 10
-	//ponieważ jest 13 czyli mamy i = 13 - 10 ==3
+
 	var i = maxMoves >10 ? maxMoves - 10  : 0;
-	console.info('Start index: ' + i);
-	maxMoves = maxMoves > 10 ? 10 : maxMoves;
-	console.info('End index: ' + maxMoves);
-	//console.info('Getting ' + orderedNumber + ' in moves:' + maxMoves);
-	
-	for(i; i <= maxMoves; i++){ //Zacznijmy od prostego max 100		
+	//maxMoves = maxMoves > 10 ? 10 : maxMoves;
+
+	for(i; i <= (maxMoves > 10 ? 10 : maxMoves); i++){ //Zacznijmy od prostego max 100	
 		var quote1 = this._registeredBrokers[0].getQuote(i * 10);
 		var quote2 = this._registeredBrokers[1].getQuote((maxMoves - i) * 10);
 		
 		var total = parseFloat((quote1 + quote2).toFixed(4));
-		
 		if(minQuote === undefined){
 			minQuote = total;
 		} else if (total < minQuote){
